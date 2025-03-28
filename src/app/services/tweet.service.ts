a8a2ee4d52de4b184d00e2a459f1e07ab2d5be78
shensim94 <shensim@oregonstate.edu>
2025-03-28 00:36:50 -0700

    try to deploy

--- src/app/services/tweet.service.ts
+++ src/app/services/tweet.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ITweet } from '../models/tweet.model';
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://twiddir-app:8080/api/v1.0/tweets'
+}
